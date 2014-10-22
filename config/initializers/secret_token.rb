4e8686338fa18d06b32c9f278c29715347a97bfd
chandantiatros <chandan.burman@tiatros.com>
2014-10-22 12:01:09 +0530

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FlowPlayer::Application.config.secret_token = '4bbda0fed8692fcdec22b3ab713a16aee939460c967a4f7dad564f93967851ba0a842ce5b7ff225039cb501059bfff4d7ad669d069b7dbb521c68fc670b31ef8'
